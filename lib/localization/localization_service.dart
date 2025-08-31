c2e2d6cdf5b1652ba83935b715c0acd64d7e57f6
Nabil134 <mohammad.nabil.ashraf@gmail.com>
2025-08-30 18:55:22 -0700

    Add loadTranslations function with string conversion for GetX

--- lib/localization/localization_service.dart
+++ lib/localization/localization_service.dart
@@ -8,12 +8,17 @@ class AppTranslations extends Translations {
   @override
   Map<String, Map<String, String>> get keys => _keys;
 
-  /// Load JSON files dynamically
+  /*loadTranslations start here*/
   Future<void> loadTranslations() async {
+    // Load English JSON
     final enJson = await rootBundle.loadString('assets/lang/en.json');
+    // Load Spanish JSON
     final esJson = await rootBundle.loadString('assets/lang/es.json');
 
-    _keys['en_US'] = Map<String, String>.from(json.decode(enJson));
-    _keys['es_ES'] = Map<String, String>.from(json.decode(esJson));
+    // Convert Json dynamic values to String for GetX because translation accept Map value in String
+    _keys['en_US'] = Map<String, String>.from(json.decode(enJson).map((key,value)=>MapEntry(key, value.toString(),),),);
+    _keys['es_ES'] = Map<String, String>.from(json.decode(esJson).map((key,value)=>MapEntry(key, value.toString(),),),);
+    // Convert Json dynamic values to String for GetX because translation accept Map value in String
   }
+/*loadTranslations end here*/
 }
